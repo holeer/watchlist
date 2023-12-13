93088af57854b237f31759026dda5aeff7c6a97a
holer <9085595+holeer@user.noreply.gitee.com>
2023-12-13 23:03:45 +0800

    ch5

--- app.py
+++ app.py
@@ -1,25 +1,69 @@
 from flask import Flask,url_for,render_template
 from markupsafe import escape
+from flask_sqlalchemy import SQLAlchemy
+import os
+import sys
+import click
 
-name = 'Grey Li'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
+WIN = sys.platform.startswith('win')
+if WIN: prefix = 'sqlite:///'
+else: prefix = 'sqlite:////'
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+db = SQLAlchemy(app)  # 初始化扩展，传入程序实例 app
+
+class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(20))  # 名字
+
+class Movie(db.Model):  # 表名将会是 movie
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    title = db.Column(db.String(60))  # 电影标题
+    year = db.Column(db.String(4))  # 电影年份
+
+@app.cli.command()  # 注册为命令，可以传入 name 参数来自定义命令
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+def initdb(drop):
+    if drop:  # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')  # 输出提示信息
+
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+
+    # 全局的两个变量移动到这个函数内
+    name = 'Grey Li'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
 
 @app.route('/')
 def index():
-    return render_template('index.html', name=name, movies=movies)
+    user = User.query.first()  # 读取用户记录
+    movies = Movie.query.all()  # 读取所有电影记录    
+    return render_template('index.html', user=user, movies=movies)
 
 @app.route('/user/<name>')
 def user_page(name):
@@ -36,3 +80,26 @@ def test_url_for():
     # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
     return 'Test page'
+
+def test_db_write():
+    user = User(name='Grey Li')  # 创建一个 User 记录
+    m1 = Movie(title='Leon', year='1994')  # 创建一个 Movie 记录
+    m2 = Movie(title='Mahjong', year='1996')  # 再创建一个 Movie 记录
+    db.session.add(user)  # 把新创建的记录添加到数据库会话
+    db.session.add(m1)
+    db.session.add(m2)
+    db.session.commit()  # 提交数据库会话，只需要在最后调用一次即可  
+
+def test_db_update():
+    movie = Movie.query.get(2)
+    movie.title = 'WALL-E'  # 直接对实例属性赋予新的值即可
+    movie.year = '2008'
+    db.session.commit()  # 注意仍然需要调用这一行来提交改动     
+
+def test_db_delete():
+    movie = Movie.query.get(1)
+    db.session.delete(movie)  # 使用 db.session.delete() 方法删除记录，传入模型实例
+    db.session.commit()  # 提交改动    
+
+if __name__ == '__main__': 
+    pass
